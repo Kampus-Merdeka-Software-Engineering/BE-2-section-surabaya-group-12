a4ac148cf44f945eea02392d65dd340b1d6a50fa
rizkydharma02 <rizkydharma02@gmail.com>
2023-12-06 12:20:45 +0700

    merge branch development and migrate prisma orm to railway

--- prisma/migrations/20231206051841_init/migration.sql
+++ prisma/migrations/20231206051841_init/migration.sql
@@ -2,8 +2,8 @@
 CREATE TABLE `About` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `about_fullname` VARCHAR(191) NOT NULL,
-    `about_position` VARCHAR(191) NOT NULL,
+    `about_fullname` VARCHAR(50) NOT NULL,
+    `about_position` VARCHAR(50) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -12,8 +12,8 @@ CREATE TABLE `About` (
 CREATE TABLE `Room` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `room_type` VARCHAR(191) NOT NULL,
-    `room_price` VARCHAR(191) NOT NULL,
+    `room_type` VARCHAR(50) NOT NULL,
+    `room_price` VARCHAR(50) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -22,10 +22,10 @@ CREATE TABLE `Room` (
 CREATE TABLE `Contact` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `contact_fullname` VARCHAR(191) NOT NULL,
-    `contact_email` VARCHAR(191) NOT NULL,
-    `contact_phone` INTEGER NOT NULL,
-    `contact_message` VARCHAR(191) NOT NULL,
+    `contact_fullname` VARCHAR(50) NOT NULL,
+    `contact_email` VARCHAR(50) NOT NULL,
+    `contact_phone` BIGINT NOT NULL,
+    `contact_message` VARCHAR(500) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -34,8 +34,8 @@ CREATE TABLE `Contact` (
 CREATE TABLE `Customer` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `customer_name` VARCHAR(191) NOT NULL,
-    `customer_description` VARCHAR(191) NOT NULL,
+    `customer_name` VARCHAR(50) NOT NULL,
+    `customer_description` VARCHAR(500) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
